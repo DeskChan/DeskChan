2174d8ccd701261e210ff1053f4307c9d7a78345
Ivan Kulikov <gunmetal313@users.noreply.github.com>
2018-01-03 15:38:17 +0300

    LimitHashMap: LimitHashMap was fixed.

--- src/main/java/info/deskchan/core_utils/LimitHashMap.java
+++ src/main/java/info/deskchan/core_utils/LimitHashMap.java
@@ -12,12 +12,14 @@ public class LimitHashMap<K,V> extends HashMap<K,V> {
         limit = maxCapacity;
     }
     public V put(K key, V value){
-        if(!containsKey(key))
-            while(orderedKeySet.size()>=limit){
-                remove(orderedKeySet.get(0));
-                orderedKeySet.remove(0);
-            }
-
+        if (size()>=limit)
+        {
+            K removeKey = orderedKeySet.remove(0);
+            this.remove(removeKey);
+        }
+        if(!containsKey(key)){
+            orderedKeySet.add(key);
+        }
         return super.put(key, value);
     }
 }
